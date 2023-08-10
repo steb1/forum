224918ff4a0082dd10f967736e0b2594877c9ebc
fmamadou <mmmfall73@gmail.com>
2023-08-10 17:22:18 +0000

    10 mock category

--- data/sql/insert.sql
+++ data/sql/insert.sql
@@ -7,3 +7,15 @@ DELETE FROM comment_like;
 DELETE FROM comment;
 DELETE FROM post_category;
 DELETE FROM category;
+
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("0","Culture",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("1","Sport",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("2","Education",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("3","Movie",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("4","Game",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("5","Dance",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("6","Musique",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("7","Art",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("8","Information",now(),now())
+INSERT INTO category(id,name,createDate,modifiedDate) VALUES ("9","Actuality",now(),now())
+
