9098c6f6d7cfa14b0803ce33bbe5356e54d4866b
fmamadou <mmmfall73@gmail.com>
2023-08-10 18:02:30 +0000

    10 mocks posts

--- data/sql/insert.sql
+++ data/sql/insert.sql
@@ -29,3 +29,15 @@ INSERT INTO user(id,username,email,password, avatarURL,type,token,tokenExpiratio
 INSERT INTO user(id,username,email,password, avatarURL,type,token,tokenExpirationDate) VALUES ("7","bouba","bouba@gmail.com","$2a$04$FiWWNzKyobHYajGiKEoyquXSNsWiIYX.zfUTtx.VY6HhFhNBXbgGG","upload/avatar.7.jpeg","2")
 INSERT INTO user(id,username,email,password, avatarURL,type,token,tokenExpirationDate) VALUES ("8","abdou","abdou@gmail.com","$2a$04$FiWWNzKyobHYajGiKEoyquXSNsWiIYX.zfUTtx.VY6HhFhNBXbgGG","upload/avatar.8.jpeg","2")
 INSERT INTO user(id,username,email,password, avatarURL,type,token,tokenExpirationDate) VALUES ("9","ndeye","ndeye@gmail.com","$2a$04$FiWWNzKyobHYajGiKEoyquXSNsWiIYX.zfUTtx.VY6HhFhNBXbgGG","upload/avatar.9.jpeg","2")
+
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("0","bon","good vibe","upload/avatar.0.jpeg","0","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("1","bon","good vibe","upload/avatar.1.jpeg","9","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("2","bon","good vibe","upload/avatar.0.jpeg","0","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("3","bon","good vibe","upload/avatar.2.jpeg","1","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("4","bon","good vibe","upload/avatar.6.jpeg","4","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("5","bon","good vibe","upload/avatar.7.jpeg","5","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("6","bon","good vibe","upload/avatar.8.jpeg","5","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("7","bon","good vibe","upload/avatar.3.jpeg","3","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("8","bon","good vibe","upload/avatar.4.jpeg","4","false",now(),now())
+INSERT INTO post(id,title,description,imageURL,authorID,isEdited,createDate,modifiedDate) VALUES ("9","bon","good vibe","upload/avatar.5.jpeg","8","false",now(),now())
+
