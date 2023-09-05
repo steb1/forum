1d96a2a406cb77dab04cce8a927cb7157f75a03d
pba <alassanebv23@gmail.com>
2023-09-05 20:04:44 +0000

    config.sh

--- push.sh
+++ push.sh
@@ -1,8 +1,4 @@
 git add .
-read -r -p "Your email: " email
-read -r -p "Your name: " mail
-git config --global user.email "$email"
-git config --global user.name "$mail"
 read -r -p "Your commit message: " message
 git commit -m "$message"
 git push
